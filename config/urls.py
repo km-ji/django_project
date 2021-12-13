e12a50b27c56acd0191939f3b49a1dc788dc138e
km-ji <90051822+mjikim@users.noreply.github.com>
2021-12-14 03:26:27 +0900

    Django Project

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('home.urls')),
+    path('polls/', include('polls.urls')),
+    path('bookmark/', include('bookmark.urls')),
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+
+]
